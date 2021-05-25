e9d7174764574817d12b07f7ab696fd0af12599f
ayush7 <ayushyadav173@gmail.com>
2021-05-25 05:34:30 +0530

    Completed Project MVP

---
+++ DNA_to_Protein.py
@@ -0,0 +1,42 @@
+print("Starting sequencing")
+
+input_file = 'example.txt'
+f = open(input_file,'r')
+seq = f.read()
+seq = seq.replace("\n","")
+seq = seq.replace("\r","")
+seq = seq.upper()
+
+def translate(seq):
+    table = {
+        'ATA':'I', 'ATC':'I', 'ATT':'I', 'ATG':'M',
+        'ACA':'T', 'ACC':'T', 'ACG':'T', 'ACT':'T',
+        'AAC':'N', 'AAT':'N', 'AAA':'K', 'AAG':'K',
+        'AGC':'S', 'AGT':'S', 'AGA':'R', 'AGG':'R',                 
+        'CTA':'L', 'CTC':'L', 'CTG':'L', 'CTT':'L',
+        'CCA':'P', 'CCC':'P', 'CCG':'P', 'CCT':'P',
+        'CAC':'H', 'CAT':'H', 'CAA':'Q', 'CAG':'Q',
+        'CGA':'R', 'CGC':'R', 'CGG':'R', 'CGT':'R',
+        'GTA':'V', 'GTC':'V', 'GTG':'V', 'GTT':'V',
+        'GCA':'A', 'GCC':'A', 'GCG':'A', 'GCT':'A',
+        'GAC':'D', 'GAT':'D', 'GAA':'E', 'GAG':'E',
+        'GGA':'G', 'GGC':'G', 'GGG':'G', 'GGT':'G',
+        'TCA':'S', 'TCC':'S', 'TCG':'S', 'TCT':'S',
+        'TTC':'F', 'TTT':'F', 'TTA':'L', 'TTG':'L',
+        'TAC':'Y', 'TAT':'Y', 'TAA':'_', 'TAG':'_',
+        'TGC':'C', 'TGT':'C', 'TGA':'_', 'TGG':'W',
+    }
+
+    protein_seq = ""
+    strip_length = (len(seq)%3)*-1
+
+    if strip_length != 0:
+        seq = seq[:strip_length]
+    
+    for i in range(0, len(seq),3):
+        codon = seq[i:i+3]
+        protein_seq = protein_seq + table[codon]
+    
+    print(protein_seq)
+
+translate(seq)
